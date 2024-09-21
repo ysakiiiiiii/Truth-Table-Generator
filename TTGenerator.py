535f296d7e61b96f10ba3e2d677ab0addb707032
FraN-onLine <151704994+FraN-onLine@users.noreply.github.com>
2024-09-21 15:48:22 +0800

    Update TTGenerator.py

--- TTGenerator.py
+++ TTGenerator.py
@@ -1,10 +1,34 @@
-statement = input("Enter Statement")
+connectives = ["not", "and", "or", "implies"] #bicon idk
 
-if statement == "P AND Q":
-    print("P     | Q     | P AND Q")
-    print("-----------------------")
-    print("True  | True  | Re-yal")
-    print("True  | False | Fak-eh")
-    print("False | True  | False")
-    print("False | False | False")
-#joke lng eto, not sure about the implementation 
+statement = input("Enter a statement: ")
+
+words = statement.split()
+
+variables = []
+connectivesUsed = []
+
+for word in words:
+    if word.isalpha() and word not in connectives:  
+        variables.append(word)
+    elif word in connectives:  
+        connectivesUsed.append(word)
+        
+conCount = 0
+varCount = 0
+
+#fck this logic
+while varCount < len(variables) or conCount < len(connectivesUsed):
+    if conCount < len(connectivesUsed) and connectivesUsed[conCount] == "not":
+        print(connectivesUsed[conCount] + " ")
+        print(variables[varCount] + " ")
+        conCount += 1
+        varCount += 1
+    elif varCount < len(variables) and varCount <= conCount:
+        print(variables[varCount] + " ")
+        varCount += 1
+    
+    if conCount < len(connectivesUsed):
+        print(connectivesUsed[conCount] + " ")
+        conCount += 1 
+    
+        
