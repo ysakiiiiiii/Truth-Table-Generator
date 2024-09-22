e61e12cf4a2d7337ee395e36de549d2976680a2b
Franaffel <151704994+FraN-onLine@users.noreply.github.com>
2024-09-22 09:25:07 +0800

    Update TTGenerator.py

--- TTGenerator.py
+++ TTGenerator.py
@@ -1,5 +1,5 @@
 connectives = ["not", "and", "or", "implies"] #bicon idk
-
+vars = ["P", "Q", "R"]
 statement = input("Enter a statement: ")
 
 words = statement.split()
@@ -7,28 +7,32 @@ words = statement.split()
 variables = []
 connectivesUsed = []
 
+#reads variables and connectives
 for word in words:
-    if word.isalpha() and word not in connectives:  
+    if word.isalpha() and word in vars:  
         variables.append(word)
     elif word in connectives:  
         connectivesUsed.append(word)
+    else 
+        print("invalid syntax detected")
+        #it does nothing yet tamad ako dh
         
 conCount = 0
 varCount = 0
 
-#fck this logic
+#reads logic
 while varCount < len(variables) or conCount < len(connectivesUsed):
     if conCount < len(connectivesUsed) and connectivesUsed[conCount] == "not":
-        print(connectivesUsed[conCount] + " ")
+        print(connectivesUsed[conCount] + " ")#to be substituted with actual logi 
         print(variables[varCount] + " ")
         conCount += 1
         varCount += 1
     elif varCount < len(variables) and varCount <= conCount:
-        print(variables[varCount] + " ")
+        print(variables[varCount] + " ")#to be substituted with actual logic
         varCount += 1
     
     if conCount < len(connectivesUsed):
-        print(connectivesUsed[conCount] + " ")
+        print(connectivesUsed[conCount] + " ")#to be substituted with actual logic
         conCount += 1 
     
         
