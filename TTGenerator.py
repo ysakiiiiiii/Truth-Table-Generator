69fba58baf2aaf4ba5e86f745742d5ec967e71b3
Franaffel <151704994+FraN-onLine@users.noreply.github.com>
2024-09-22 10:18:48 +0800

    Update TTGenerator.py

--- TTGenerator.py
+++ TTGenerator.py
@@ -9,37 +9,52 @@ words = statement.split()
 variables = []
 connectivesUsed = []
 
+
+
 #reads variables and connectives
 for word in words:
-    if word.isalpha() and word in vars:  
-        variables.append(word)
-    elif word in connectives:  
-        connectivesUsed.append(word)
-    else:
-        print("invalid syntax detected")
-        valid = False
+ if word.isalpha() and word in vars:  
+  variables.append(word)
+ elif word in connectives:  
+  connectivesUsed.append(word)
+ else:
+  print("invalid syntax detected")
+  valid = False
         #it does nothing yet tamad ako eh
-        #this does not validate double connectives except not
-        #double variables
-        #only detects elements not present in the lists
+        
+ for word1, word2 in zip(words, words[1:]):
+    if valid:
+     if word1 == word2:
+         print("Invalid Statement") 
+         valid = False
+         break
+     if word1 in vars and word2 in vars:
+         print("Invalid Statement")
+         valid = False
+         break
+     if word1 in connectives and word2 in connectives and word1 != "not":
+         print("Invalid Statement")
+         valid = False
+         break
+        
+        
 
-while valid: #(tab niyo yung sa baba)
-conCount = 0
-varCount = 0
+if valid: #(tab niyo yung sa baba)
+ conCount = 0
+ varCount = 0
 
 #reads logic
-while varCount < len(variables) or conCount < len(connectivesUsed):
-    if conCount < len(connectivesUsed) and connectivesUsed[conCount] == "not":
-        print(connectivesUsed[conCount] + " ")#to be substituted with actual logi 
-        print(variables[varCount] + " ")
-        conCount += 1
-        varCount += 1
-    elif varCount < len(variables) and varCount <= conCount:
-        print(variables[varCount] + " ")#to be substituted with actual logic
-        varCount += 1
+ while varCount < len(variables) or conCount < len(connectivesUsed):
+     if conCount < len(connectivesUsed) and connectivesUsed[conCount] == "not":
+         print(connectivesUsed[conCount] + " ")#to be substituted with actual logi 
+         print(variables[varCount] + " ")
+         conCount += 1
+         varCount += 1
+     elif varCount < len(variables) and varCount <= conCount:
+         print(variables[varCount] + " ")#to be substituted with actual logic
+         varCount += 1
     
-    if conCount < len(connectivesUsed):
-        print(connectivesUsed[conCount] + " ")#to be substituted with actual logic
-        conCount += 1 
+     if conCount < len(connectivesUsed):
+         print(connectivesUsed[conCount] + " ")#to be substituted with actual logic
+         conCount += 1 
     
-        
